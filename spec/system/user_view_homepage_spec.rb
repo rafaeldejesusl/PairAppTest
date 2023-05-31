9e5e8fe801eec8c9d0a83072c1dc88a585ea2d10
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2023-05-31 16:02:47 -0300

    remocao de comentarios do teste

--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -2,12 +2,9 @@ require 'rails_helper'
 
 describe 'Usuário visita tela inicial' do
   it 'e vê o nome do aplicativo' do
-    # Arrange
-
-    # Act
     visit root_path
 
-    # Assert
     expect(page).to have_content 'Hello World'
+    expect(current_path).to eq root_path
   end
 end
